55229325c5d370b0f5e637164c04d7b14c1da2d7
Edward Joel | Automation Developer <Joeledward949@gmail.com>
2026-01-17 13:51:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script type="module">
+
 /* ================= FIREBASE IMPORTS ================= */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
